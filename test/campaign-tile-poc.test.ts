18d01fc376b25ea5076866538ce4475b9ee91106
charltona <2724511+charltona@users.noreply.github.com>
2024-06-03 20:51:55 +1000

    Initial commit

---
+++ test/campaign-tile-poc.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CampaignTilePoc from '../lib/campaign-tile-poc-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/campaign-tile-poc-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CampaignTilePoc.CampaignTilePocStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
