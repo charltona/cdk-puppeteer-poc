b5f4c7fcac587e9852976331297a02d438927ad9
charltona <2724511+charltona@users.noreply.github.com>
2024-06-07 11:44:58 +1000

    Add CDK stack, packages, and ts config

--- lib/campaign-tile-poc-stack.ts
+++ lib/campaign-tile-poc-stack.ts
@@ -1,16 +1,41 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as nodejs from 'aws-cdk-lib/aws-lambda-nodejs'
+import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as path from "path";
+import { CfnOutput, Duration } from "aws-cdk-lib";
 
 export class CampaignTilePocStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const chromiumLayer = new lambda.LayerVersion(this, 'ChromiumLayer', {
+      code: lambda.Code.fromAsset(path.join(__dirname, '../lambda-layer')),
+      compatibleRuntimes: [lambda.Runtime.NODEJS_18_X],
+      description: 'Chromium lambda layer',
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CampaignTilePocQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const pupetteerFunction = new nodejs.NodejsFunction(this, 'Puppet', {
+      entry: path.join(__dirname, "../src/handler.ts"),
+      handler: "handler",
+      bundling: {
+        externalModules:[
+          "aws-sdk",
+          "@aws-sdk/*"
+        ]
+      },
+      runtime: lambda.Runtime.NODEJS_18_X,
+      layers: [chromiumLayer],
+      timeout: Duration.minutes(2),
+      memorySize: 512,
+    })
+
+    const fnUrl = pupetteerFunction.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE
+    })
+
+    new CfnOutput(this, 'FnUrl', {
+      value: fnUrl.url
+    })
   }
 }
